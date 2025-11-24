731238ec2d49522ed5fdf610ef7c25675cc1bca9
Jhonnatanmartins <jhonnatanmartins7@gmail.com>
2025-11-24 02:11:04 +0000

    Add files via upload

---
+++ Attempt_1.py
@@ -0,0 +1,16 @@
+def count_by(x, n):
+    if x > n:
+        x - n
+    return 2
+
+print(count_by(9, 3))
+print("hello_world")
+if 5 > 2 :
+    print(five greater than two)
+if 5 > 2
+    print(Five is greater than two)
+X=5
+Y= hello world
+Print (x)
+print (y)
+    
